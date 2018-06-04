1f74cc8adee6dea81c5dc9817ef7c5179ff57583
charleswoolfork <charleswoolfork@aol.com>
2018-06-03 22:13:16 -0400

    first commit

---
+++ CWVirtualPet2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This part allows the user to give their name and to name their pet while assigning random numbers to fields.
+            Random random = new Random();
+            int Hunger = random.Next(90, 100);
+            int Thirst = random.Next(50, 70);
+            int Bathroom = random.Next(40, 80);
+            int Energy = random.Next(50, 80);
+            int Affection = random.Next(65, 100);
+
+            //PROPERTIES
+            VirtualPet userPup = new VirtualPet();
+            userPup.Hunger = Hunger;
+            userPup.Thirst = Thirst;
+            userPup.BathroomNeed = Bathroom;
+            userPup.Energy = Energy;
+            userPup.Affection = Affection;
+
+            //UI
+            Console.WriteLine("Welcome to the Virtual Pet Application!\n Please tell me your name.");
+            string ownersName = Console.ReadLine();
+            userPup.OwnersName = ownersName;
+            Console.Clear();
+            Console.WriteLine("What would you like to name your pet?");
+            userPup.PetsName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Congratulations {0}!\nYou now have {1} a virtual puppy!", userPup.OwnersName, userPup.PetsName);
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            //This section does work until the puppy reaches a happines level of at least 90. Then user can quit at anytime.
+            do
+            {
+
+                userPup.GetStatus(); // "TICK"
+
+                //USER MENU
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("\tPress 1 to feed {0}.", userPup.PetsName);
+                Console.WriteLine("\tPress 2 to give {0} water.", userPup.PetsName);
+                Console.WriteLine("\tPress 3 to take {0} outside to go to the bathroom.", userPup.PetsName);
+                Console.WriteLine("\tPress 4 to rub {0}'s belly.", userPup.PetsName);
+                Console.WriteLine("\tPress 5 to play with {0}.", userPup.PetsName);
+                Console.WriteLine("\tPress 6 to quit the game.");
+                int userChoice = int.Parse(Console.ReadLine());
+
+                while (userChoice < 1 || userChoice > 6)
+                {
+                    Console.WriteLine("You must choose 1 - 6 from the menu!\nWhat do you want to do for your puppy?");
+                    userChoice = int.Parse(Console.ReadLine());
+                }
+
+
+                switch (userChoice)
+                {
+
+                    case 1:
+                        {
+                            userPup.FeedPuppy();
+                        }
+                        break;
+
+                    case 2:
+                        {
+                            userPup.WaterPuppy();
+                        }
+                        break;
+
+                    case 3:
+                        {
+                            userPup.Bathroom();
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            userPup.RubPuppy();
+                        }
+                        break;
+
+                    case 5:
+                        {
+                            userPup.PlayWithPuppy();
+                        }
+                        break;
+
+                    case 6:
+                        {
+                            Console.WriteLine("{0} says, \"What a lazy owner!\"", userPup.PetsName);
+                            Console.WriteLine("Your puppy is now a stray");
+                            Environment.Exit(0);
+                        }
+                        break;
+
+                    default: //the while loop above controls this, default is an extra safe guard that will simply feed the puppy. 
+                        {
+                            Console.WriteLine("{0)} says, \"Thank you for the food!\"", userPup.PetsName);
+                            userPup.FeedPuppy();
+                            break;
+                        }
+
+
+                }
+                Console.WriteLine("Press any key to continue");
+                Console.ReadLine();
+
+
+            } while (userPup.PetHappiness <= 75);
+
+            //this section is what prints to the console when the virtual puppy's satisfaction is greater than  or equal to 75).
+
+            Console.WriteLine("\nCongratulations {0}!\nYou have made {1} very happy today!\nThanks for playing Virtual Puppy!", userPup.OwnersName, userPup.PetsName);
+
+        }//MAIN
+
+    }//CLASS
+
+}//NAMESPACE
---
+++ CWVirtualPet2/VirtualPet.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        // DECLARING FIELDS
+
+        string ownersName;
+        string petsName;
+        int hunger;
+        int thirst;
+        int bathroomNeed;
+        int energy;
+        int affection;
+
+        //DECLARING CONSTRUCTORS
+
+        public VirtualPet()//DEFAULT CONSTRUCTOR
+        {
+
+        }
+        public VirtualPet(string ownersName, string petsName)//LOADED CONSTRUCTOR 1
+        {
+            this.ownersName = ownersName;
+            this.petsName = petsName;
+        }
+        public VirtualPet(int hunger, int thirst, int bathroom, int energy, int affection)//LOADED CONSTRUCTOR 2
+        {
+            this.hunger = hunger;
+            this.thirst = thirst;
+            this.bathroomNeed = bathroom;
+            this.energy = energy;
+            this.affection = affection;
+        }
+
+
+        // DECLARING PROPERTIES
+        public string OwnersName
+        {
+            get { return this.ownersName; }
+            set { this.ownersName = value; }
+        }
+
+
+        public string PetsName
+        {
+            get { return this.petsName; }
+            set { this.petsName = value; }
+        }
+
+        public int Hunger
+        {
+            get
+            {
+                return this.hunger;
+
+            }
+            set
+            {
+                this.hunger = value;
+                if (this.hunger < 0)
+                {
+                    this.hunger = 0;
+                }
+                if (this.hunger > 100)
+                {
+                    this.hunger = 100;
+                }
+            }
+
+        }
+        public int Thirst
+        {
+            get { return this.thirst; }
+
+            set
+            {
+                this.thirst = value;
+                if (this.thirst < 0)
+                {
+                    this.thirst = 0;
+                }
+                if (this.thirst > 100)
+                {
+                    this.thirst = 100;
+                }
+            }
+        }
+
+        public int BathroomNeed
+        {
+            get { return this.bathroomNeed; }
+
+            set
+            {
+                this.bathroomNeed = value;
+                if (this.bathroomNeed < 0)
+                {
+                    this.bathroomNeed = 0;
+                }
+                if (this.bathroomNeed > 100)
+                {
+                    this.bathroomNeed = 100;
+                }
+            }
+        }
+
+        public int Energy
+        {
+            get { return this.energy; }
+
+            set
+            {
+                this.energy = value;
+                if (this.energy < 0)
+                {
+                    this.energy = 0;
+                }
+                if (this.energy > 100)
+                {
+                    this.energy = 100;
+                }
+            }
+        }
+        public int Affection
+        {
+            get { return this.affection; }
+
+            set
+            {
+                this.affection = value;
+                if (this.affection < 0)
+                {
+                    this.affection = 0;
+                }
+                if (this.affection > 100)
+                {
+                    this.affection = 100;
+                }
+            }
+        }
+
+        public double PetHappiness //this property takes the average of the virtual puppy's needs and subtracts it from 100 to get the pet's level of happiness. It will be used in main to determine the end of the game. 
+        {
+            get { return 100 - ((this.Hunger + this.Thirst + this.BathroomNeed + this.Affection + this.Energy) / 5); }
+
+        }
+
+
+
+
+
+        public void GetStatus()
+        {
+            Console.WriteLine("{0}  the virtual puppy:", this.PetsName);
+            Console.WriteLine("\tis {0}% hungry.", this.Hunger);
+            Console.WriteLine("\tis {0}% thirsty.", this.Thirst);
+            Console.WriteLine("\thas a {0}% need to go to the bathroom.", this.BathroomNeed);
+            Console.WriteLine("\tis {0}% energetic.", this.Energy);
+            Console.WriteLine("\tis {0}% lonely.", this.Affection);
+            Console.WriteLine();
+        }
+
+
+        //DECLARING METHODS
+
+        public void FeedPuppy()
+        {
+            this.Hunger -= 50;
+            this.BathroomNeed += 20;
+            this.Energy += 10;
+            this.Thirst += 30;
+
+            Console.WriteLine("{0} says, \"Bacon! Nom Nom Nom!\"", this.PetsName);
+        }
+
+        public void WaterPuppy()
+        {
+            this.Thirst -= 80;
+            this.BathroomNeed += 20;
+
+            Console.WriteLine("{0} says, \"Bark! Bark! Bark!\"", this.PetsName);
+        }
+
+        public void Bathroom()
+        {
+            this.BathroomNeed -= 90;
+            this.Energy -= 10;
+
+            Console.WriteLine("{0} says, \"Puppy pees on tree!\"", this.PetsName);
+        }
+
+        public void RubPuppy()
+        {
+            this.Energy -= 30;
+            this.Affection -= 40;
+            this.BathroomNeed += 10;
+
+            Console.WriteLine("{0} says, \"Puppy licks your face!\"", this.PetsName);
+        }
+
+        public void PlayWithPuppy()
+        {
+            this.Energy -= 60;
+            this.Affection -= 60;
+
+            Console.WriteLine("{0} says, \"Bark! Bark! Bark!\"", this.PetsName);
+        }
+
+    }//CLASS
+}//NAMESPACE
